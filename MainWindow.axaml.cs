5cbe93d60b92941571852fc0fd1fed6234a78b5e
W1stash <154755772+W1stash@users.noreply.github.com>
2024-05-18 21:01:25 +0300

    Add files via upload

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace _4_3_app 
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
